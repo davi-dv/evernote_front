8791ab5a1cab7ccd9fb17b3136b98ff715e1dea6
davi <davisilva@amo.delivery>
2022-01-10 10:24:14 -0300

    criado card da tela de registro com todo o html e estilizaçao, agora falta apenas o form e o botao de registro que vai ser retornado da api

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,18 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomeScreen from "./screens/home"
-import RegisterScren from './screens/auth/register'
-import LoginScren from './screens/auth/login'
+import RegisterScreen from './screens/auth/register'
+import LoginScreen from './screens/auth/login'
 import NotesIndexScreen from './screens/notes/index'
 import UserEditScreen from './screens/users/edit'
 const appRoutes = () => (
     <BrowserRouter>
         <Routes>
             <Route exact path="/" element={<HomeScreen/>} />
-            <Route exact path="/" element={<RegisterScren/>} />
-            <Route exact path="/" element={<LoginScren/>} />
-            <Route exact path="/" element={<NotesIndexScreen/>} />
-            <Route exact path="/" element={<UserEditScreen/>} />
+            <Route exact path="/register" element={<RegisterScreen/>} />
+            <Route exact path="/login" element={<LoginScreen/>} />
+            <Route exact path="/notes" element={<NotesIndexScreen/>} />
+            <Route exact path="/user/edit" element={<UserEditScreen/>} />
         </Routes>
     </BrowserRouter>
 )
